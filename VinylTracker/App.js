893cc2cd89309b0efdd4d3c4fc246f2ffed0023c
Tristan Caetano <tristancostacaetano@gmail.com>
2024-03-11 02:24:27 -0400

    Learning more from tutorial, beginning first screen.

--- VinylTracker/App.js
+++ VinylTracker/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { func } from "prop-types";
+import React from "react";
+import { View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+    return(
+        <View style = {{
+            backgroundColor: "white",
+            flex: 1,
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+            flexWrap: "wrap",
+        }}>
+            <View style={{
+                backgroundColor: "blue",
+                width: 100,
+                height: 100,
+                // alignSelf: "flex-start",
+            }}/>
+            <View style={{
+                backgroundColor: "gold",
+                width: 100,
+                height: 100,
+            }}/>
+            <View style={{
+                backgroundColor: "tomato",
+                width: 100,
+                height: 100,
+            }}/>
+            <View style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+            }}/>
+            <View style={{
+                backgroundColor: "green",
+                width: 100,
+                height: 100,
+            }}/>
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ VinylTracker/app/assets/screens/WelcomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ImageBackground } from 'react-native/types';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground source={require('')}></ImageBackground>
+    );
+}
+
+export default WelcomeScreen;
